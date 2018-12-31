3e6f4ba22c5fc5ed0edc2e11e19f9628da52ed58
Joram Barrez <jbarrez@users.noreply.github.com>
2018-12-31 15:15:51 +0100

    Rename micronaut output jar

--- micronaut-sample/build-native-image.sh
+++ micronaut-sample/build-native-image.sh
@@ -1,16 +1,16 @@
 ./mvnw package
-java -cp target/my-app-0.1.jar io.micronaut.graal.reflect.GraalClassLoadingAnalyzer
+java -cp target/micronaut-sample-app-1.0-SNAPSHOT.jar io.micronaut.graal.reflect.GraalClassLoadingAnalyzer
 native-image --no-server \
              --allow-incomplete-classpath \
              --delay-class-initialization-to-runtime=sun.font.SunLayoutEngine \
              --delay-class-initialization-to-runtime=io.netty.handler.ssl.util.BouncyCastleSelfSignedCertGenerator \
              --delay-class-initialization-to-runtime=io.netty.handler.ssl.JdkNpnApplicationProtocolNegotiator \
              --delay-class-initialization-to-runtime=io.netty.handler.ssl.ReferenceCountedOpenSslEngine \
-             --class-path target/my-app-0.1.jar \
+             --class-path target/micronaut-sample-app-1.0-SNAPSHOT.jar \
              -H:ReflectionConfigurationFiles=target/reflect.json \
              -H:EnableURLProtocols=http \
              -H:IncludeResources="logback.xml|application.yml|META-INF/services/*.*" \
-             -H:Name=my-app \
+             -H:Name=micronaut-sample-app \
              -H:Class=org.flowable.app.Application \
              -H:+ReportUnsupportedElementsAtRuntime \
              -H:+AllowVMInspection \

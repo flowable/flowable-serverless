0c2d995963af919737a6e1364a4ba51dc20168f3
Filip Hrisafov <filip.hrisafov@gmail.com>
2019-02-16 09:20:35 +0100

    Spring Fu sample with Graal

--- spring-fu-sample/build-graal-image.sh
+++ spring-fu-sample/build-graal-image.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 
-mvn package
-# ../jafu-reactive-minimal/gradlew clean build -Pgraal=true
-# unzip build/libs/jafu-reactive-minimal.jar -d build/libs/jafu-reactive-minimal
+mvn clean package -Pgraal
+unzip target/flowable-function-spring-fu-sample*.jar -d target/flowable-function-spring-fu-sample
 
-native-image -H:IncludeResources='META-INF/.*.json|META-INF/spring.factories|org/springframework/boot/logging/.*' --allow-incomplete-classpath --delay-class-initialization-to-runtime=io.netty.handler.codec.http.HttpObjectEncoder,org.springframework.core.io.VfsUtils,org.springframework.format.support.DefaultFormattingConversionService -H:ReflectionConfigurationFiles=graal/app.json,graal/boot.json,graal/framework.json,graal/log4j.json,graal/netty.json/custom.reflect.json -Dio.netty.noUnsafe=true -H:+ReportUnsupportedElementsAtRuntime -Dfile.encoding=UTF-8 -cp ".:$(echo build/libs/jafu-reactive-minimal/BOOT-INF/lib/*.jar | tr ' ' ':')":build/libs/jafu-reactive-minimal/BOOT-INF/classes com.sample.Application
+native-image -H:IncludeResources='META-INF/.*.json|META-INF/spring.factories|org/springframework/boot/logging/.*' --allow-incomplete-classpath --delay-class-initialization-to-runtime=io.netty.handler.codec.http.HttpObjectEncoder,org.springframework.core.io.VfsUtils,org.springframework.format.support.DefaultFormattingConversionService -H:ReflectionConfigurationFiles=graal/app.json,graal/boot.json,graal/framework.json,graal/log4j.json,graal/netty.json,graal/custom-reflect.json -Dio.netty.noUnsafe=true -H:+ReportUnsupportedElementsAtRuntime -Dfile.encoding=UTF-8 -cp ".:$(echo target/flowable-function-spring-fu-sample/BOOT-INF/lib/*.jar | tr ' ' ':')":target/flowable-function-spring-fu-sample/BOOT-INF/classes org.flowable.sample.SpringFuApplication
